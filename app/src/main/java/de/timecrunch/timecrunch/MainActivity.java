2542b913cfa2682bc9295a2754586081478a991f
Daniel Lange <daniel.lange2@gmail.com>
2018-11-08 23:07:54 +0100

    Added dayview and materialcalendar

--- app/src/main/java/de/timecrunch/timecrunch/MainActivity.java
+++ app/src/main/java/de/timecrunch/timecrunch/MainActivity.java
@@ -3,14 +3,49 @@ package de.timecrunch.timecrunch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCalendarView mcv;
+    private ArrayList<String> mHours = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mcv = findViewById(R.id.calendarView);
+        mcv.setTopbarVisible(false);
+
+        initHours();
+
         Intent intent = new Intent(this, TodoCategoriesActivity.class);
         startActivity(intent);
     }
+
+    private void initHours() {
+        for(int i = 0; i < 24; i++) {
+            if(i < 10) {
+                mHours.add("0" + String.valueOf(i) + ":00");
+            } else {
+                mHours.add(String.valueOf(i) + ":00");
+            }
+        }
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView rv = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mHours);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
---
+++ app/src/main/java/de/timecrunch/timecrunch/RecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package de.timecrunch.timecrunch;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mHours = new ArrayList<String>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> hours) {
+        mContext = context;
+        mHours = hours;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_hour, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder vh, int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        vh.hourText.setText(mHours.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHours.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout hourLayout;
+        TextView hourText;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            hourLayout = itemView.findViewById(R.id.hour_layout);
+            hourText = itemView.findViewById(R.id.hour_text);
+        }
+    }
+
+}
