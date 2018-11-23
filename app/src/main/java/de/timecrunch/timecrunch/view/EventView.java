94fe48aaf634a73f5a7f74fdca7b0c980a90efdd
Daniel Lange <daniel.lange2@gmail.com>
2018-11-23 11:13:26 +0100

    Changed color, added 15 Minute steps, added text

--- app/src/main/java/de/timecrunch/timecrunch/view/EventView.java
+++ app/src/main/java/de/timecrunch/timecrunch/view/EventView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -17,9 +18,12 @@ public class EventView extends View {
     // 54 - 1 = 0.75h
     // 72 - 1 = 1.00h
     private final int SQUARE_SIZE = dpToPx(71);
+    private static final int PADDING = 60;
 
     private Rect mRectSquare;
     private Paint mPaintSquare;
+    private Paint mPaintBorder;
+    private Paint mPaintText;
 
     public EventView(Context context) {
         super(context);
@@ -44,7 +48,17 @@ public class EventView extends View {
     private void init(@Nullable AttributeSet set) {
         mRectSquare = new Rect();
         mPaintSquare = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintSquare.setColor(Color.GREEN);
+        mPaintSquare.setStyle(Paint.Style.FILL);
+        mPaintSquare.setColor(Color.parseColor("#FF58D903"));
+
+        mPaintBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBorder.setStyle(Paint.Style.STROKE);
+        mPaintBorder.setColor(Color.parseColor("#FF44A703"));
+        mPaintBorder.setStrokeWidth(2);
+
+        mPaintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintText.setColor(Color.BLACK);
+        mPaintText.setTextSize(35);
     }
 
     private int offsetX = 0;
@@ -55,15 +69,18 @@ public class EventView extends View {
         super.onDraw(canvas);
 
         if(offsetY == 0 && offsetX == 0) {
-            setRectBounds(mRectSquare, SQUARE_SIZE, SQUARE_SIZE);
+            setRectBounds(mRectSquare, dpToPx(PADDING - 11), dpToPx(19));
         }
         canvas.drawRect(mRectSquare, mPaintSquare);
+        canvas.drawRect(mRectSquare, mPaintBorder);
+
+        canvas.drawText("TEST", (mRectSquare.width() / 2) + 80, mRectSquare.top + mRectSquare.height() / 2, mPaintText);
     }
 
     private void setRectBounds(Rect r, int left, int top) {
         r.left = left;
         r.top = top;
-        r.right = r.left + SQUARE_SIZE;
+        r.right = r.left + getWidth() - dpToPx(PADDING);
         r.bottom = r.top + SQUARE_SIZE;
     }
 
@@ -74,9 +91,11 @@ public class EventView extends View {
         return Math.round((float) dp * density);
     }
 
+    float oldY = 0.0f;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         boolean value = super.onTouchEvent(event);
+
 
         switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
@@ -97,14 +116,37 @@ public class EventView extends View {
                 float x = event.getX();
                 float y = event.getY();
 
-                setRectBounds(mRectSquare, (int) x - ((mRectSquare.bottom - mRectSquare.top) / 2),
-                        (int) y - ((mRectSquare.right - mRectSquare.left) / 2));
+                if (oldY == 0) {
+                    oldY = y;
+                }
+
+                int DISTANCE = dpToPx(18);
+
+                if (y > oldY + DISTANCE) {
+                    setRectBounds(mRectSquare,
+                            dpToPx(PADDING - 11),
+                            mRectSquare.top + DISTANCE);
+                    // (int) y - ((mRectSquare.bottom - mRectSquare.top) / 2));
+                    oldY = y;
+                } else if (y < (oldY - DISTANCE)) {
+                    setRectBounds(mRectSquare,
+                            dpToPx(PADDING - 11),
+                            mRectSquare.top - DISTANCE);
+                    // (int) y - ((mRectSquare.bottom - mRectSquare.top) / 2));
+                    oldY = y;
+                }
+
+                Log.d(" ", "oldY: " + String.valueOf(oldY));
+                Log.d(" ", "Y: " + String.valueOf(y));
+
+
                 postInvalidate();
 
                 return true;
             }
             case MotionEvent.ACTION_UP: {
                 getParent().requestDisallowInterceptTouchEvent(false);
+                oldY = 0;
             }
         }
         return value;
