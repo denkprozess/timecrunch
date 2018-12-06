33f8130204fcb24acd0ff2858da4ec2d5a9eefdc
Lasse Hammer <lasse.hammer@live.de>
2018-12-06 23:51:49 +0100

    Added Removing of Tasks by swiping left

--- app/src/main/java/de/timecrunch/timecrunch/view/TaskOverviewFragment.java
+++ app/src/main/java/de/timecrunch/timecrunch/view/TaskOverviewFragment.java
@@ -5,18 +5,24 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -115,8 +121,11 @@ public class TaskOverviewFragment extends Fragment {
         Category idCategory = new Category(categoryId, null, 0, false);
         List<Task> taskList = taskMap.get(idCategory);
         if (taskList != null) {
+            TaskListAdapter adapter = new TaskListAdapter(taskList);
             taskListView.setLayoutManager(new LinearLayoutManager(getContext()));
-            taskListView.setAdapter(new TaskListAdapter(taskList));
+            taskListView.setAdapter(adapter);
+            ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwipeToDeleteCallback(adapter, this.getContext()));
+            itemTouchHelper.attachToRecyclerView(taskListView);
         }
     }
 
@@ -206,9 +215,38 @@ public class TaskOverviewFragment extends Fragment {
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
-            Category idCategory = new Category(categoryId, null, 0, false);
-            for(String text: strings) {
-                taskViewModel.addTask(idCategory, new Task(1, text));
+            for (String text : strings) {
+                taskViewModel.addTask(categoryId, new Task(1, text));
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            hideProgressBar();
+        }
+
+    }
+
+    private class RemoveTaskAsync extends AsyncTask<Task, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showProgressBar();
+        }
+
+
+        @Override
+        protected Void doInBackground(Task... tasks) {
+//            try {
+//                Thread.sleep(3000);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+            for (Task task : tasks) {
+                taskViewModel.removeTask(categoryId, task);
             }
             return null;
         }
@@ -222,4 +260,55 @@ public class TaskOverviewFragment extends Fragment {
     }
 
 
+
+    private class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
+        private TaskListAdapter adapter;
+        private Drawable icon;
+        private ColorDrawable background;
+
+        public SwipeToDeleteCallback(TaskListAdapter adapter, Context context) {
+            super(0, ItemTouchHelper.LEFT);
+            this.adapter = adapter;
+            this.icon = ContextCompat.getDrawable(context,
+                    R.drawable.ic_delete_white_24dp);
+            this.background = new ColorDrawable(Color.RED);
+        }
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+            int position = viewHolder.getAdapterPosition();
+            Task deletedTask = adapter.deleteItem(position);
+            new RemoveTaskAsync().execute(deletedTask);
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            View itemView = viewHolder.itemView;
+            int backgroundCornerOffset = 20;
+
+
+            int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+            int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+            int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+            if (dX < 0) { // Swiping to the left
+                int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+                int iconRight = itemView.getRight() - iconMargin;
+                icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+                background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                        itemView.getTop(), itemView.getRight(), itemView.getBottom());
+            } else { // view is unSwiped
+                background.setBounds(0, 0, 0, 0);
+            }
+            background.draw(c);
+            icon.draw(c);
+        }
+    }
 }
